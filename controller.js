8bdc6969f9f472abc37397b51afa5e7012643f80
Jon Sadka <jonsadka@gmail.com>
2014-11-04 20:55:16 -0800

    implement angular cdn

--- controller.js
+++ controller.js
@@ -1,11 +1,13 @@
 app.controller('recursionCtrl',['$scope', 'recursiveFactory', function($scope, recursiveFactory){
-    $scope.getData = function(){
+    $scope.description = "See the scripts below";
+    $scope.pricing = [];
+    $scope.circles = [1,2,3,4,5,6];
+    $scope.getPricingData = function(){
       recursiveFactory.fetch().then(function(result){
+        $scope.pricing = result;
         console.log('Got the data!', result);
       }, function(error){
         console.log('Oh no, I have an error', error);
       })
     };
-    $scope.description = "See the scripts below";
-    $scope.circles = [1,2,3,4,5,6];
   }])
