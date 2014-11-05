09b554d36ab0578f1646824c7a814e604658ef22
Jon Sadka <jonsadka@gmail.com>
2014-11-04 20:23:23 -0800

    get data button inplemented

--- controller.js
+++ controller.js
@@ -1,4 +1,11 @@
-app.controller('recursionCtrl',['$scope', function($scope){
+app.controller('recursionCtrl',['$scope', 'recursiveFactory', function($scope, recursiveFactory){
+    $scope.getData = function(){
+      recursiveFactory.fetch().then(function(result){
+        console.log('Got the data!', result);
+      }, function(error){
+        console.log('Oh no, I have an error', error);
+      })
+    };
     $scope.description = "See the scripts below";
     $scope.circles = [1,2,3,4,5,6];
   }])
--- factory.js
+++ factory.js
@@ -1,21 +1,18 @@
-app.factory('recursiveFactory', ['$http', function(http){
+app.factory('recursiveFactory', ['$http', '$q', function($http, $q){
+  var url =  "https://api.uber.com/v1/estimates/price?";
+  url += "start_latitude=37.625732&start_longitude=-122.377807";
+  url += "&end_latitude=37.785114&end_longitude=-122.406677";
+  url += "&server_token=Gfna7WFIVdjbi5oaaQHIav-X1X27PWyHUAaWoOqA";
 
-  var path = ''// add url here
-
-  return {
-    init: function(jsonData){
-
-      // do logic
-
-      var promise = http.get( path, jsonData, {
-
-        headers: {
-          
-        }
-
-      })
-
-    }
+  var fetch = function(){
+    console.log(url)
+    var deferred = $q.defer();
+    $http.get(url).success(deferred.resolve).error(deferred.reject);
+    return deferred.promise;
   };
 
-})
+  return {
+    fetch: fetch
+  };
+
+}]);
