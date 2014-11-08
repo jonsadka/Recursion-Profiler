4a2e4f014292a47f4306e4349496a6b8bef81b3e
Jon Sadka <jonsadka@gmail.com>
2014-11-07 16:56:15 -0800

    further updates to directive

--- directive.js
+++ directive.js
@@ -11,10 +11,13 @@ app.directive('bubbleChartDirective', [function(){
     var svg = d3.select(el).append('svg');
     var g = svg.append('g');
 
+    var bubbles = g.selectAll('.bubblesInChart');
+
     // Update elements on resize of page
     scope.$watch(function(){
       return el.clientWidth * el.clientHeight;
     }, function(){
+      console.log('Window Resized');
       width = el.clientWidth;
       height = el.clientHeight;
 
@@ -23,11 +26,14 @@ app.directive('bubbleChartDirective', [function(){
       svg.attr({width: width, height: height});
 
       g.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+      bubbles.attr({'r': 100, 'cx':100, 'cy':100, 'class':'bubblesInChart'});
     });
 
     // Function when data is added or removed
     scope.$watch('data', function(data){
-      console.log('data changed');
+      console.log('Data changed');
+      // bubbles.enter().append('circle')
     });
 
   }
